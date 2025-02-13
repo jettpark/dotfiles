935cd83b8da2649d753ec052b679576998eef627
gyuannn <gyuannn1100@gmail.com>
2025-02-14 00:59:33 +0800

    fix: set vim conceal to 0 for markdown files

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Dotfiles 管理
+# Dotfiles
 
 ## 安装 stow
 
-```
+```bash
 brew install stow
 ```
 
@@ -10,7 +10,7 @@ brew install stow
 
 需要先备份原有的默认配置文件，再依次执行
 
-```
+```bash
 stow --adopt -nv nvim  # 先测试 symbol links 会如何建立
 
 stow --adopt -v nvim # 建立 symbol links
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 解决 Mardown ``` 不能正常显示的问题
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
