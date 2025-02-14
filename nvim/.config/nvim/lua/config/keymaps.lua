8e2a840ce0a87461b179a416bb76b26e01d123e4
gyuannn <gyuannn1100@gmail.com>
2025-02-14 22:09:18 +0800

    fix: 解决了一些 tmux 和 neovim 的默认配置问题

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 禁用 Ecs 的 Meta 能力，防止快速按 Esc + j 出现行交换的情况
+vim.keymap.del("v", "<M-j>")
+vim.keymap.del("v", "<M-k>")
+vim.keymap.del("n", "<M-j>")
+vim.keymap.del("n", "<M-k>")
+vim.keymap.del("i", "<M-j>")
+vim.keymap.del("i", "<M-k>")
